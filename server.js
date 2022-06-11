6bf50550fe3f4e228460d3b216154729977b987f
theopappas17 <theopappas17@gmail.com>
2022-06-10 22:29:28 -0400

    Minimum capability.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const api = require("./routes/index.js");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,10 +10,12 @@ app.use(express.json());
 //Middleware for parsing urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", api);
-
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
